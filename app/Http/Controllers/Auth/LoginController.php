52c65b5b1c44a71bada7118dafe13758ac76570b
dmprocode <danielmathias2022@gmail.com>
2026-09-13 00:47:46 +0300

    Resolving file conflict

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -77,12 +77,6 @@ class LoginController extends Controller
     $request->session()->regenerate();
 
     // 7. Redirect based on role
-<<<<<<< HEAD
-    if ($admin->role === 'admin') {
-        return redirect()->route('admin-dashboard')
-            ->with('success', 'Welcome, ' . $admin->fname);
-    }
-=======
    
     if ($admin->role === 'super_admin') {
        return redirect()->route('super-adimin')->with('success','welcame');
@@ -92,7 +86,6 @@ class LoginController extends Controller
     }
 
     
->>>>>>> temp-branch
 
     return redirect()->route('home')->with('success', 'Login successful!');
 }
